abe0e051810a9c07670bf53cb98f5a5c226292d1
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2019-12-17 13:20:57 +0100

    Add files via upload

---
+++ EjemploMutex/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Cliente Esperando");
+            Boolean mutexcreado;
+            Mutex mt = new Mutex(false, "nuevomutex", out mutexcreado);
+            if (!mutexcreado)
+            {
+                mt = Mutex.OpenExisting("nuevomutex");
+                mt.WaitOne();
+                mt.ReleaseMutex();
+            }
+           Console.WriteLine("Cliente Leyendo");
+           StreamReader sr = new StreamReader("D:\\fichero.txt");
+           String pal = sr.ReadLine();
+            while (pal.CompareTo("$")!=0)
+            {
+                Console.WriteLine(pal);
+                pal = sr.ReadLine();
+            }
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EjemploMutex/EjemploMutex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+namespace EjemploMutex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("d:\\fichero.txt");
+            Mutex mt = new Mutex(false, "nuevomutex");
+            mt.WaitOne();
+            String pal;
+             Console.WriteLine("Introduce palabra");
+             pal = Console.ReadLine();
+             while(pal.CompareTo("$")!=0)
+             {
+                 sw.WriteLine(pal);
+                 Console.WriteLine("Introduce palabra");
+                 pal = Console.ReadLine();
+             }
+             sw.WriteLine("$");
+             sw.Close();
+            Console.ReadLine();
+            mt.ReleaseMutex();
+            Console.ReadLine();
+         
+        }
+    }
+}
