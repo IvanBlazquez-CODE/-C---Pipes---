205cb12d495a6d51ffd792e8d23ba25255340a20
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2019-12-17 13:19:10 +0100

    Add files via upload

---
+++ EjemploSemaforos/EjemploSemaforos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EjemploSemaforos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Semaphore s = new Semaphore(4, 4, "semaforo");
+            Console.WriteLine("Semáforo iniciado");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EjemploSemaforos/Semaforofrm/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace Semaforofrm
+{
+    public partial class Form1 : Form
+    {
+        Semaphore s;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s.Release();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            s = Semaphore.OpenExisting("semaforo");
+            s.WaitOne();
+        }
+    }
+}
