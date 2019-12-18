1a490bd6bd515b3384beb4bc767f917f6de68ce7
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2019-12-18 20:48:45 +0100

    CSharp

---
+++ ThrowException/ThrowException/ThrowException/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estadistica e = new Estadistica();
+            try
+            {
+
+                //e.add(7);
+                //e.add(5);
+
+                Console.WriteLine(e.getResultado().ToString());
+                Console.ReadLine();
+            }
+            catch(MuestraVaciaException ex)
+            {
+              
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine(); 
+            }
+
+            short i;
+           checked
+            {
+                for (i = 0; i <= short.MaxValue; i++)
+                {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+            Console.ReadLine(); 
+
+        }
+    }
+}
