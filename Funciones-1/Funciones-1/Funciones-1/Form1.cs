0e08a8f7c712d8a479523655c01e256487788b82
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2019-12-18 20:30:48 +0100

    CSharp

---
+++ Funciones-1/Funciones-1/Funciones-1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Funciones_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(txtNum.Text);
+            txtFact.Text = Globales.factorial(num).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(txtNum.Text);
+            int res;
+            Globales.factorial(num, out res);
+            txtFact.Text = res.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(txtNum.Text);
+            Globales.factorial(ref num);
+            txtFact.Text = num.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int x;
+            Globales.maximo(out x,1, 2,5,199,2,49);
+            MessageBox.Show(x.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Globales.buscar(3,0).ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(txtNum.Text);
+            txtFact.Text = Globales.factorialRec(num).ToString();
+            Globales.factorial(3);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int max, min;
+            float media;
+            int[] tabla = { 1, 2, 3, 4, 5 };
+            Globales.funcionEstadistica(tabla, out max, out min, out media);
+            MessageBox.Show(String.Format("{0},{1},{2}", max.ToString(), min.ToString(), media.ToString()));
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (Globales.buscarLetra('s', "cese"))
+                MessageBox.Show("Letra encontrada");
+            else
+                MessageBox.Show("Letra no encontrada");
+        }
+    }
+}
---
+++ Funciones-1/Funciones-1/Funciones-1/Globales.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones_1
+{
+    class Globales
+    {
+        public static int factorial(int x)
+        {
+            int res = 1;
+            int i;
+            for (;x>1 ;x--)
+            {
+                res *= x;
+            }
+            return res;
+        }
+
+        public static void factorial(int x, out int res)
+        {
+            res = 1;
+            int i;
+            for (i = 2; i <= x; i++)
+            {
+                res *= i;
+            }
+        }
+
+        public static void factorial(ref int x)
+        {
+            int res = 1;
+            int i;
+            for (i = 2; i <= x; i++)
+            {
+                res *= i;
+            }
+            x = res;
+        }
+        public static void  maximo(out int res,params int [] num)
+        {
+            res = num[0];
+            int i;
+            for (i = 0; i < num.Length; i++)
+                if (num[i] > res) res = num[i];
+         
+        }
+        public static int buscar(int x, int pos=0,params int []num)
+        {
+            int i;
+            for (i = pos; i < num.Length; i++)
+            {
+                if (num[i] == x) return i;
+            }
+
+            return -1;
+        }
+        public static int suma(int x=1,int y=1)
+        {
+            return x + y;
+           
+        }
+        public static int factorial(int num,int max=10)
+        {
+            int res = 1;
+            int i;
+            for (i = 2; i <= num && res<=max; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+        public static int factorialRec(int x)
+        {
+            if (x == 0 || x == 1)
+                return 1;
+            else
+                return x * factorialRec(x - 1);
+        }
+
+        public static void funcionEstadistica(int []tabla,out int max, out int min, out float media)
+        {
+            int i,tam;
+            max = 0;
+            min = 0;
+            media = tabla[0];
+            tam = tabla.Length;
+            for (i=1;i<tam;i++)
+            {
+                if (tabla[i] > tabla[max])
+                    max = i;
+                else if (tabla[i] < tabla[min])
+                    min = i;
+                media += tabla[i]; 
+
+            }
+            max = tabla[max];
+            min = tabla[min];
+            media = media /(float) tam;
+        }
+
+        public static bool buscarLetra(char letra, String frase)
+        {
+            if (frase.Length==1)
+            {
+                if (letra == frase[0])
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                return letra == frase[0] || buscarLetra(letra, frase.Substring(1));
+            }
+
+        }
+    }   
+}
