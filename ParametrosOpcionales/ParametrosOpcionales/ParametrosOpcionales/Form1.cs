9335334ecfdce14e471902aa05679d3263be711d
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2019-12-18 20:33:19 +0100

    CSharp

---
+++ ParametrosOpcionales/ParametrosOpcionales/ParametrosOpcionales/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParametrosOpcionales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Funciones.suma(3,2,1).ToString());
+        }
+    }
+}
