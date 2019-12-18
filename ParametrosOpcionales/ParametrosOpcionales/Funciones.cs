7d222b07b9a869c908007c715df8e613d300d218
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2019-12-18 19:55:14 +0100

    Add files via upload

---
+++ ParametrosOpcionales/ParametrosOpcionales/Funciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrosOpcionales
+{
+    public class Funciones
+    {
+
+        public static int suma(int a , int b=0)
+        {
+            return a + b;
+        }
+        public static int suma(params int []numeros)
+        {
+            int suma = 0;
+            for (int i = 0; i < numeros.Length; i++)
+                suma += 2*numeros[i];
+            return suma;
+        }
+        public static int suma(int a)
+        {
+            return a ;
+        }
+    }
+}
