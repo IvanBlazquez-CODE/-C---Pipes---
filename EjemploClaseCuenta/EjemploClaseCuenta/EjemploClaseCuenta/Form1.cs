ba436cdf07e5d10dbfff2de8b37242f63cb9576a
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2019-12-18 20:06:18 +0100

    CSharp

---
+++ EjemploClaseCuenta/EjemploClaseCuenta/EjemploClaseCuenta/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploClaseCuenta
+{
+    public partial class Form1 : Form
+    {
+        Cuenta c = new Cuenta("Ejemplo");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c.Ingresar(float.Parse(txtCantidad.Text));
+            txtSaldo.Text = c.getSaldo().ToString();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            c.Extraer(float.Parse(txtCantidad.Text));
+            txtSaldo.Text=c.getSaldo().ToString();
+        }
+    }
+}
