d7094b6aa78aa4d12987ed562e206db308a975ee
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2019-12-18 20:18:16 +0100

    CSharp

---
+++ Enumeracion/Enumeracion/Enumeracion/Enumeracion/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enumeracion
+{
+    public enum dias
+    {
+        lunes, martes, miercoles, jueves, viernes
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dias d;
+            for (d=dias.lunes;d<=dias.viernes;d++)
+            {
+                comboBox1.Items.Add(d.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num;
+            
+            if ( int.TryParse(textBox1.Text, out num))
+            {
+                MessageBox.Show("Correcto");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+
+
+        }
+    }
+}
